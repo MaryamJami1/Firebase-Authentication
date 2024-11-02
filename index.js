c2cd74f317277c1167042637aa317048074005f2
MaryamJami1 <maryamchouhan373@gmail.com>
2024-11-02 20:26:27 +0500

    sign up form

--- index.js
+++ index.js
@@ -19,14 +19,65 @@ btn.addEventListener("click", () => {
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                switch (errorMessage) {
+                    case "Firebase: Password should be at least 6 characters (auth/weak-password).":
+                        Swal.fire({
+                            icon: "error",
+                            title: "Weak Password",
+                            text: "Password should be at least 8 characters long."
+                        });
+                        break;
+
+                    case "Firebase: Error (auth/invalid-email).":
+                        Swal.fire({
+                            icon: "error",
+                            title: "Invalid Email",
+                            text: "Please enter a valid email address."
+                        });
+                        break;
+
+                    case "Firebase: Error (auth/email-already-in-use).":
+                        Swal.fire({
+                            icon: "error",
+                            title: "Email already Exists",
+                            text: "If you are already registered, please SignIn"
+                        });
+                        break;
+                }
                 console.log(errorMessage);
+
 
             })
 
     }
 
     else {
-        console.log("insert your data");
+        Swal.fire({
+            icon: "error",
+            title: "Fill out the fields",
+
+        });
 
     }
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "center",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "success",
+        title: "Sign up successfully"
+    });
+
+
+    setTimeout(() => {
+        location.href = "login.html"
+    }, 2000)
 })
