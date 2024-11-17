db6d891dfbb62e3d70054f71b818a44d676b5153
MaryamJami1 <maryamchouhan373@gmail.com>
2024-11-17 23:41:05 +0500

    post

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-import { getAuth, createUserWithEmailAndPassword, doc, setDoc, db, getDoc, updateDoc, serverTimestamp } from "./firebase.js";
+import { getAuth, createUserWithEmailAndPassword, doc, setDoc, db, getDoc, updateDoc } from "./firebase.js";
 
 let btn = document.getElementById("register")
 let email = document.getElementById("accountEmail")
@@ -12,9 +12,75 @@ const auth = getAuth();
 btn.addEventListener("click", async () => {
     if (email.value.trim() && password.value.trim()) {
         createUserWithEmailAndPassword(auth, email.value, password.value)
-            .then((userCredential) => {
+            .then(async (userCredential) => {
                 const user = userCredential.user;
                 console.log(user);
+
+                try {
+                    const user = auth.currentUser;
+                    if (user) {
+                        /////get uid
+                        const uid = user.uid;
+                        console.log(uid);
+
+                        //////add user in database with id
+                        await setDoc(doc(db, "usersWithId", uid), {
+                            name: name.value,
+                            address: Address.value,
+                            phoneNo: phoneNum.value,
+                            email: email.value,
+                        });
+                        console.log("Document written with ID: ", uid);
+
+
+
+
+                        // //////////////////update data with time
+                        // const updateTimestamp = await updateDoc(docRef, {
+                        //     timestamp: serverTimestamp()
+                        // });
+
+                        //////////read user data
+                        //    const docRef = doc(db, "usersWithId", uid);
+                        //    const docSnap = await getDoc(docRef);
+                        //         if (docSnap.exists()) {
+                        //             console.log("Document data:", docSnap.data());
+                        //         } else {
+                        //             console.log("No such document!");
+                        //         }                           
+
+
+                    } else {
+                        console.log("no user login");
+                    }
+
+
+
+
+                    //////////////////set doc with static id
+
+                    // await setDoc(doc(db, "userId", "098647"), {
+                    //     name: name.value,
+                    //     address: Address.value,
+                    //     phoneNo:phoneNum.value
+                    //   });
+                    //   console.log("Document written with ID: ");
+
+
+
+                    ///////////////////   get doc
+
+                    // const docRef = await addDoc(collection(db, "users"), {
+                    //   name: name.value,
+                    //   address: Address.value,
+                    //   phoneNo:phoneNum.value
+                    // });
+                } catch (e) {
+                    console.error("Error adding document: ", e);
+                }
+
+
+
                 const Toast = Swal.mixin({
                     toast: true,
                     position: "center",
@@ -33,67 +99,6 @@ btn.addEventListener("click", async () => {
 
 
                 setTimeout(async () => {
-
-                    try {
-                        const user = auth.currentUser;
-                        if (user) {
-                            /////get uid
-                            const uid = user.uid;
-                            console.log(uid);
-
-                            //////add user in database with id
-                            await setDoc(doc(db, "usersWithId", uid), {
-                                name: name.value,
-                                address: Address.value,
-                                phoneNo: phoneNum.value
-                            });
-                             console.log("Document written with ID: ", uid);
-
-                             
-                            const docRef = doc(db, "usersWithId", uid);
-                            const docSnap = await getDoc(docRef);
-
-                            //////////////////update data with time
-                            const updateTimestamp = await updateDoc(docRef, {
-                                timestamp: serverTimestamp()
-                            });
-
-                       //////////read user data
-                            if (docSnap.exists()) {
-                                console.log("Document data:", docSnap.data());
-                            } else {
-                                console.log("No such document!");
-                            }                           
-
-
-                        } else {
-                            console.log("no user login");
-                        }
-
-
-                       
-
-                        //////////////////set doc with static id
-
-                        // await setDoc(doc(db, "userId", "098647"), {
-                        //     name: name.value,
-                        //     address: Address.value,
-                        //     phoneNo:phoneNum.value
-                        //   });
-                        //   console.log("Document written with ID: ");
-
-
-
-                        ///////////////////   get doc
-
-                        // const docRef = await addDoc(collection(db, "users"), {
-                        //   name: name.value,
-                        //   address: Address.value,
-                        //   phoneNo:phoneNum.value
-                        // });
-                    } catch (e) {
-                        console.error("Error adding document: ", e);
-                    }
                     location.href = "login.html"
                 }, 1000)
 
--- profilepage.js
+++ profilepage.js
@@ -1,19 +1,71 @@
-import { doc, getAuth, setDoc, getDocs, db, serverTimestamp, addDoc, collection, query, where, onSnapshot,updateDoc } from "./firebase.js";
+import { doc, getAuth, getDoc, getDocs, db, serverTimestamp, addDoc, collection, query, where, onSnapshot, updateDoc } from "./firebase.js";
 let postBtn = document.getElementById("postBtn");
 let postText = document.getElementById("postText");
 let getPost = document.getElementById("getPost");
 let getDiv = document.getElementById("getDiv");
+let userName = document.getElementById("name");
+let userAddress = document.getElementById("address")
+let userNum = document.getElementById("phoneNum")
+let useremail = document.getElementById("email")
+
+
+
+
+/////////////// get user info quesries
+const userRef = collection(db, "userWithId");
+const u = query(userRef);
+
+const unsub = onSnapshot(u, async() => {
+    console.log("calling")
+    
+    ////////read user data
+    const user = auth.currentUser;
+    console.log(user);
+    const uid = user.uid;
+    console.log(uid);
+
+    const docRef = doc(db, "usersWithId", uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        console.log(docSnap.data());
+
+        let name = docSnap.data().name;
+        userName.innerText = name;
+
+        let address = docSnap.data().address;
+        userAddress.innerHTML = address;
+
+        let number = docSnap.data().phoneNo;
+        userNum.innerHTML = number
+
+        let email = docSnap.data().email;
+        useremail.innerHTML = email;
+
+
+    } else {
+        console.log("No such document!");
+    }
+
+});
+
+
+
+
+
+
 
 
 const auth = getAuth();
 postBtn.addEventListener("click", async () => {
     const user = auth.currentUser;
+    console.log(user);
+
     if (user) {
         /////get uid
         const uid = user.uid;
         console.log(uid);
 
-        // add data with random id
+        ////// add data with random id
         try {
             const docRef = await addDoc(collection(db, "posts"), {
                 post: postText.value,
@@ -30,18 +82,27 @@ postBtn.addEventListener("click", async () => {
     }
 
 
-})
+});
+
+
+
+
 
 ////////////////// get post by id
 getPost.addEventListener("click", async () => {
-    const q = query(collection(db, "posts"), where("id", "==", "stZykA7b0KX7wS37ELWZtcjqZF13"));
+    const user = auth.currentUser;
+    console.log(user);
+    const uid = user.uid;
+    console.log(uid);
+    const q = query(collection(db, "posts"), where("id", "==", uid));
 
     try {
         const querySnapshot = await getDocs(q);
-        getDiv.innerHTML =" ";
+        getDiv.innerHTML = " ";
         querySnapshot.forEach((doc) => {
-         
-            getDiv.innerHTML += `<div>${doc.data().post}</div> </br> </br>`
+
+            getDiv.innerHTML += `<div>${doc.data().post}</div> </br></br> `
+
         });
     } catch (e) {
         console.log(e);
@@ -49,27 +110,26 @@ getPost.addEventListener("click", async () => {
     }
 
 
-})
+});
 
 
 
-///////////////// queries for posts
+
+
+///////////////// queries for get all posts
 const usersRef = collection(db, "posts");
 
-const q = query(usersRef, where("id", "==", "stZykA7b0KX7wS37ELWZtcjqZF13"))
-const unsubscribe = onSnapshot(q, (querySnapshot) => {
+const q = query(usersRef)
+const unsubscribe = onSnapshot(q, async (querySnapshot) => {
     console.log("calling");
-    const user = auth.currentUser;
-    const uid = user.uid;
-    const washingtonRef = doc(db, "posts", uid);
 
+    /////// post
     querySnapshot.forEach(async (doc) => {
 
         const querySnapshot = await getDocs(q);
-        getDiv.innerHTML +=`<div>${postText.value}</div>`;
-        postText.value=" "
-    
-       
-
+        getDiv.innerHTML += `<div>${postText.value}</div>`;
+        postText.value = " "
     });
+
+
 });
